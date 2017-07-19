22893eb1e9ca10e764c14676dd89b5ed6a5fbcd7
nine-god <191176233@qq.com>
2017-07-19 22:30:38 +0800

    首页幻灯片完成

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,43 +1,20 @@
-$(document).ready(function(){
-	// alert("1111")
-	var slidey =$('.banner').unslider({
-		speed: 500,               //  The speed to animate each slide (in milliseconds)
-		delay: 1000,              //  The delay between slide animations (in milliseconds)
-		complete: function() {
-		},  //  A function that gets called after every slide animation
-		keys: true,               //  Enable keyboard (left, right) arrow shortcuts
-		dots: true,               //  Display dot navigation
-		fluid: true              //  Support responsive design. May break non-responsive designs
-	});
-
-	 data = slidey.data('unslider');
-	function hello(){
+// $(document).ready(function(){
+// 	// alert("1111")
 
 
-		//  Start Unslider
-		data.start();
-	} 
+// });
 
-	window.setInterval(hello,2000); 
-});
+// // $(document).on "turbolinks:load", ->
+// //   alert "page has loaded!"
+// document.addEventListener("turbolinks:load", function() {
+// 	$('.banner').unslider({
+// 		autoplay:true, // 定义轮播是否自动执行，默认值不自动执行
+// 		speed: 500, // 每个幻灯片动画速度(以毫秒为单位)
+// 		delay: 5000, // 幻灯片动画之间的延迟(毫秒)
+// 		keys: true, // 启用键盘快捷键(左、右)箭头
+// 		nav: true,
+// 		arrows:{},
+// 		animation : "horizontal" // 规定动画效果 'horizontal', 从从左到右,'vertical'从上至下,'fade',淡入淡出
 
-
-
-//  Pause Unslider
-//data.stop();
-
-//  Move to a slide index, with optional callback
-//data.move(2, function() {});
-//  data.move(0);
-
-//  Manually enable keyboard shortcuts
-//data.keys();
-
-//  Move to the next slide (or the first slide if there isn't one)
-//data.next();
-
-//  Move to the previous slide (or the last slide if there isn't one)
-//data.prev();
-
-//  Append the navigation dots manually
-//data.dots();
+// 	});
+// })
--- app/assets/javascripts/blog.coffee
+++ app/assets/javascripts/blog.coffee
@@ -6,5 +6,15 @@
 //= require bootstrap
 //= require unslider-min
 //= require turbolinks
-//= require app
 
+$(document).on "turbolinks:load", ->
+	$('.banner').unslider(
+		autoplay:true # 定义轮播是否自动执行，默认值不自动执行
+		speed: 500 # 每个幻灯片动画速度(以毫秒为单位)
+		delay: 5000 # 幻灯片动画之间的延迟(毫秒)
+		keys: true # 启用键盘快捷键(左、右)箭头
+		nav: true
+		arrows:{}
+		animation : "horizontal" # 规定动画效果 'horizontal', 从从左到右,'vertical'从上至下,'fade',淡入淡出
+
+	)
