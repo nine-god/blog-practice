186413db73d8719867f61dcba593a57c85e5acd2
nine-god <191176233@qq.com>
2017-07-25 21:54:31 +0800

    优化文章bootstrap wysiwyg 编辑器 对于空格，并修改首行缩进的样式

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -52,20 +52,27 @@
   	$('#article_text').html($('#editor').html());
 
   };
-  function html_encode(str) {
-    var s = "";
-    if (str.length == 0)
-        return "";
-    s = str.replace(/&/g, "&gt;");
-    s = s.replace(/</g, "&lt;");
-    s = s.replace(/>/g, "&gt;");
-    s = s.replace(/ /g, "&nbsp;");
-    s = s.replace(/\'/g, "&#39;");
-    s = s.replace(/\"/g, "&quot;");
-    s = s.replace(/\n/g, "<br>");
 
-    return s;
-}
+
+  // function synchronization(){
+  // 	var str = $('#editor').html();
+
+  // 	$('#article_text').html(html_encode(str));
+  // };
+//   function html_encode(str) {
+//     var s = "";
+//     if (str.length == 0)
+//         return "";
+//     // s = str.replace(/&/g, "&gt;");
+//     // s = s.replace(/</g, "&lt;");
+//     // s = s.replace(/>/g, "&gt;");
+//     // s = s.replace(/ /g, "&nbsp;");
+//     // s = s.replace(/\'/g, "&#39;");
+//     // s = s.replace(/\"/g, "&quot;");
+//     // s = s.replace(/\n/g, "<br>");
+//     s = str.replace(/ /g, "&nbsp;");
+//     return s;
+// }
 
 function html_decode(str) {
     var s = "";
@@ -74,7 +81,7 @@ function html_decode(str) {
     // s = str.replace(/&gt;/g, "&");
     s = str.replace(/&lt;/g, "<");
     s = s.replace(/&gt;/g, ">");
-    s = s.replace(/&nbsp;/g, " ");
+    // s = s.replace(/&nbsp;/g, " ");
     s = s.replace(/&#39;/g, "\'");
     s = s.replace(/&quot;/g, "\"");
     s = s.replace(/<br>/g, "\n");
